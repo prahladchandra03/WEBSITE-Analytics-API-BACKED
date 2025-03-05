2c689b616322b2e89bf4e3dc7e7a5a2fea5671e5
prahladhandra <prahladchandra11@gmail.com>
2025-03-05 21:32:15 +0530

    server server google login call back

--- src/config/passport.js
+++ src/config/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:'http://localhost:3000/api/auth/google/callback',
+      callbackURL:'https://website-analytics-api-1.onrender.com/api/auth/google/callback',
     },
     async (_accessToken, _refreshToken, profile, done) => {
       try {
