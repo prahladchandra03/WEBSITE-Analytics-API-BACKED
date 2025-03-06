64815087cf206d21b217c4229997adf2ae4219e9
prahladhandra <prahladchandra11@gmail.com>
2025-03-06 14:22:06 +0530

    add change jwt tocken view

--- src/middleware/isAuthenticated.js
+++ src/middleware/isAuthenticated.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    const token = req.headers.authorization?.split(" ")[1]; // Extract the token from the Authorization header
+    const token = req.headers.authorization?.split(" ")[1];
+    console.log("Authorization Header:", req.headers.authorization); // Extract the token from the Authorization header
     if (!token) {
         return res.status(401).json({ message: "No token provided" });
     }
@@ -15,3 +16,4 @@ module.exports = (req, res, next) => {
         return res.status(401).json({ message: "Invalid token" });
     }
 };
+
